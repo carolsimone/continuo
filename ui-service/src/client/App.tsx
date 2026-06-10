4d7a7a76f1406b18a19f421b3b92bfa534fa7a99
“Simone <simonecarolini.sc@gmail.com>
2026-06-10 14:41:47 +0200

    feat(ui): mount ChatPanel as right-hand assistant sidebar

--- ui-service/src/client/App.tsx
+++ ui-service/src/client/App.tsx
@@ -3,17 +3,30 @@ import DashboardPage from './DashboardPage';
 import DetailPage from './DetailPage';
 import NodeDetailPage from './NodeDetailPage';
 import ReleaseDetailPage from './ReleaseDetailPage';
+import ChatPanel from './ChatPanel';
+import { useChatSocket } from './chat/useChatSocket';
 
 export default function App() {
+  const chat = useChatSocket();
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DashboardPage />} />
-        <Route path="/schedule/:name" element={<DetailPage />} />
-        <Route path="/schedule/:name/latest" element={<DetailPage mode="latest" />} />
-        <Route path="/node/:fqn" element={<NodeDetailPage />} />
-        <Route path="/releases/:id" element={<ReleaseDetailPage />} />
-      </Routes>
+      <div className="app-shell">
+        <div className="app-shell__main">
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/schedule/:name" element={<DetailPage />} />
+            <Route path="/schedule/:name/latest" element={<DetailPage mode="latest" />} />
+            <Route path="/node/:fqn" element={<NodeDetailPage />} />
+            <Route path="/releases/:id" element={<ReleaseDetailPage />} />
+          </Routes>
+        </div>
+        <ChatPanel
+          items={chat.state.items}
+          connected={chat.connected}
+          onSend={chat.send}
+          onNewChat={chat.newChat}
+        />
+      </div>
     </BrowserRouter>
   );
 }
